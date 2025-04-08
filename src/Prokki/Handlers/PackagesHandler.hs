b172cdba0e3fd8ba9dd3b6b87f95405651e50163
Arseny <arseny@protonmail.com>
2025-04-09 02:23:04 +0700

    feat: streaming

--- src/Prokki/Handlers/PackagesHandler.hs
+++ src/Prokki/Handlers/PackagesHandler.hs
@@ -2,29 +2,23 @@
 
 module Prokki.Handlers.PackagesHandler (packagesHandler) where
 
-import qualified Data.ByteString.Char8 as BS
-import qualified Data.ByteString.Lazy as LBS
-import qualified Network.HTTP.Client as HC
+import Control.Monad.Trans.Resource (runResourceT)
+import Data.ByteString.Builder (byteString)
+import Data.Conduit (runConduit, (.|))
+import qualified Data.Conduit.Combinators as DCC
 import qualified Network.HTTP.Conduit as C
-import Network.HTTP.Types (hContentLength)
-import Network.Wai (Request, Response, responseLBS)
-import Prokki.Utils (compress, getPath)
+import Network.Wai (Request, Response, responseStream)
+import Prokki.Utils (getPath)
 
 packagesHandler :: C.Manager -> Request -> IO Response
-packagesHandler manager req = do
+packagesHandler manager req = runResourceT $ do
   let path = getPath req
       url = "https://pypi.org/" ++ path
 
   request <- C.parseRequest url
-  response <- C.httpLbs request manager
-
-  let headers = HC.responseHeaders response
-      encoding = lookup "Content-Encoding" headers
-      body = HC.responseBody response
-      newBody = case encoding of
-        Just "gzip" -> compress body
-        _ -> body
-      bodyLength = LBS.length newBody
-      newHeaders = (hContentLength, BS.pack $ show bodyLength) : filter (\(h, _) -> h /= hContentLength) headers
-
-  return $ responseLBS (HC.responseStatus response) newHeaders newBody
+  response <- C.http request manager
+  let status = C.responseStatus response
+      headers = C.responseHeaders response
+  return $ responseStream status headers $ \write flush -> do
+    let bodySource = C.responseBody response
+    runConduit $ bodySource .| DCC.mapM_ (\chunk -> write (byteString chunk) >> flush)
