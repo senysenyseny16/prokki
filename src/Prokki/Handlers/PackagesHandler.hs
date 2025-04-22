2d10fc7ed79437e6707388c1b7bd79ab84c284d9
Arseny <82811840+senysenyseny16@users.noreply.github.com>
2025-04-22 19:14:16 +0700

    fix: concurrent cache usage (temporary solution)

--- src/Prokki/Handlers/PackagesHandler.hs
+++ src/Prokki/Handlers/PackagesHandler.hs
@@ -4,32 +4,21 @@
 
 module Prokki.Handlers.PackagesHandler (packagesHandler) where
 
-import qualified Control.Exception as E
-import Control.Monad.IO.Class (MonadIO, liftIO)
-import Control.Monad.IO.Unlift (withRunInIO)
 import Control.Monad.Reader (ask)
-import Data.ByteString (ByteString)
-import Data.ByteString.Builder (Builder, byteString)
-import Data.Conduit (ConduitT, ZipConduit (..), getZipConduit, runConduit, (.|))
-import qualified Data.Conduit.Combinators as CC
 import qualified Data.Text as T
 import qualified Network.HTTP.Conduit as C
-import Network.Wai (Request, Response, responseFile, responseStream)
-import Prokki.Env (Cache (..), Env (..), Index (..))
+import Network.Wai (Request, Response)
+import Prokki.Cache (respondWithCache)
+import Prokki.Env (Env (..), Index (..))
 import Prokki.Monad (ProkkiM)
 import Prokki.Utils (getPath)
-import System.Directory (createDirectoryIfMissing, doesFileExist, removeFile)
-import System.FilePath (takeDirectory, (</>))
-import System.IO (Handle, IOMode (WriteMode), hClose, openFile)
 
 packagesHandler :: Request -> ProkkiM Response
 packagesHandler req = do
   Env {..} <- ask
   let Index {..} = index
-      Cache {..} = cache
       path = getPath req
       url = indexUrl <> path
-      packagePath = cacheDir </> T.unpack path
 
   request <- C.parseRequest (T.unpack url)
   response <- C.http request {C.decompress = const False} manager
@@ -37,32 +26,4 @@ packagesHandler req = do
       headers = C.responseHeaders response
       body = C.responseBody response
 
-  isPackageCached <- liftIO $ doesFileExist packagePath
-
-  if isPackageCached
-    then do
-      pure $ responseFile status headers packagePath Nothing
-    else do
-      withRunInIO \unlift ->
-        pure $ responseStream status headers $ \rWrite rFlush -> do
-          createDirectoryIfMissing True (takeDirectory packagePath)
-          E.bracketOnError
-            (openFile packagePath WriteMode)
-            (cleanFile packagePath)
-            ( \fHandle -> do
-                E.catch
-                  (unlift $ runConduit $ body .| writeAndRespond fHandle rWrite)
-                  (\(_ :: IOError) -> cleanFile packagePath fHandle)
-                hClose fHandle
-                rFlush
-            )
-
-writeAndRespond :: (MonadIO m) => Handle -> (Builder -> IO ()) -> ConduitT ByteString o m ()
-writeAndRespond fHandle rWrite =
-  getZipConduit
-    (ZipConduit (CC.sinkHandle fHandle) *> ZipConduit (CC.mapM_ (liftIO . rWrite . byteString)))
-
-cleanFile :: FilePath -> Handle -> IO ()
-cleanFile filePath handle = do
-  hClose handle
-  removeFile filePath `E.catch` \(_ :: IOError) -> pure ()
+  respondWithCache cache (T.unpack path) status headers body
