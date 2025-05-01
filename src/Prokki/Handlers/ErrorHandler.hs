9eac39cc45f6b5c7df4bf46c02f4e3111fe493cc
Arseny <arseny@protonmail.com>
2025-05-01 18:05:00 +0700

    feat: reqLogs

--- src/Prokki/Handlers/ErrorHandler.hs
+++ src/Prokki/Handlers/ErrorHandler.hs
@@ -1,16 +1,10 @@
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE PatternSynonyms #-}
-
 module Prokki.Handlers.ErrorHandler (errorHandler) where
 
-import Colog (Message, WithLog, log, pattern W)
 import Control.Monad.IO.Class (MonadIO)
 import Network.HTTP.Types (status404)
 import Network.Wai (Request, Response, responseLBS)
-import Prokki.Utils (getPath)
 import Prelude hiding (log)
 
-errorHandler :: (MonadIO m, WithLog env Message m) => Request -> m Response
-errorHandler req = do
-  log W $ "Unexpected endpoint: " <> getPath req
+errorHandler :: (MonadIO m) => Request -> m Response
+errorHandler _ = do
   pure $ responseLBS status404 [("Content-Type", "text/plain")] "404 Not Found"
--- src/Prokki/Middleware/RequestLogger.hs
+++ src/Prokki/Middleware/RequestLogger.hs
@@ -5,23 +5,25 @@ import qualified Data.Text as T
 import Data.Text.Encoding (decodeUtf8)
 import GHC.Stack (HasCallStack, callStack)
 import Network.HTTP.Types (statusCode)
-import Network.Wai (Middleware, rawPathInfo, requestMethod, responseStatus)
+import Network.Wai (Middleware, Request, Response, httpVersion, rawPathInfo, remoteHost, requestMethod, responseStatus)
 
 logRequests :: (HasCallStack) => LogAction IO Message -> Middleware
 logRequests logAction app req respond = do
-  let method = decodeUtf8 (requestMethod req)
-      path = decodeUtf8 (rawPathInfo req)
-
   app req \res -> do
     let status = statusCode (responseStatus res)
-        text =
-          T.concat
-            [ "[HTTP] ",
-              method,
-              " ",
-              path,
-              " -> ",
-              T.pack (show status)
-            ]
-    logAction <& (Msg {msgText = text, msgSeverity = Info, msgStack = callStack})
+        severity = if status == 200 then Info else Warning
+    logAction <& (Msg {msgText = requestMsg req res, msgSeverity = severity, msgStack = callStack})
     respond res
+
+requestMsg :: Request -> Response -> T.Text
+requestMsg req res =
+  let method = decodeUtf8 (requestMethod req)
+      path = decodeUtf8 (rawPathInfo req)
+      host = stripPort $ T.pack $ show (remoteHost req)
+      status = T.pack $ show (statusCode (responseStatus res))
+      httpVer = T.pack $ show (httpVersion req)
+   in T.intercalate " " [host, method, path, httpVer, status]
+
+stripPort :: T.Text -> T.Text
+stripPort = T.takeWhile (/= ':')
+{-# INLINE stripPort #-}
