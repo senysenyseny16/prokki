2035abe4b47467e2cbe61b0cd3c0a8218f7f001b
Arseny <arseny@protonmail.com>
2025-10-21 11:23:00 +0700

    feat: client IP middleware logging

--- src/Prokki/Middleware/RequestLogger.hs
+++ src/Prokki/Middleware/RequestLogger.hs
@@ -1,11 +1,14 @@
 module Prokki.Middleware.RequestLogger (logRequests) where
 
 import Colog (LogAction, Message, Msg (..), Severity (..), (<&))
+import Control.Applicative ((<|>))
+import Data.List (find)
+import Data.Maybe (fromMaybe)
 import qualified Data.Text as T
 import Data.Text.Encoding (decodeUtf8)
 import GHC.Stack (HasCallStack, callStack)
 import Network.HTTP.Types (statusCode)
-import Network.Wai (Middleware, Request, Response, httpVersion, rawPathInfo, remoteHost, requestMethod, responseStatus)
+import Network.Wai (Middleware, Request, Response, httpVersion, rawPathInfo, remoteHost, requestHeaders, requestMethod, responseStatus)
 
 logRequests :: (HasCallStack) => LogAction IO Message -> Middleware
 logRequests logAction app req respond = app req respond'
@@ -20,10 +23,22 @@ requestMsg :: Request -> Response -> T.Text
 requestMsg req res =
   let method = decodeUtf8 (requestMethod req)
       path = decodeUtf8 (rawPathInfo req)
-      host = stripPort $ T.pack $ show (remoteHost req)
+      host = getClientIp req
       status = T.pack $ show (statusCode (responseStatus res))
       httpVer = T.pack $ show (httpVersion req)
    in T.intercalate " " [host, method, path, httpVer, status]
+
+getClientIp :: Request -> T.Text
+getClientIp req =
+  let headers = requestHeaders req
+      forwardedFor = lookup "X-Forwarded-For" headers >>= (Just . decodeUtf8)
+      realIp = lookup "X-Real-IP" headers >>= (Just . decodeUtf8)
+      hostIp = stripPort $ T.pack $ show (remoteHost req)
+   in fromMaybe hostIp $ (forwardedFor >>= getFirstIp) <|> realIp
+
+getFirstIp :: T.Text -> Maybe T.Text
+getFirstIp txt = (find (not . T.null) . map T.strip) $ T.splitOn "," txt
+{-# INLINE getFirstIp #-}
 
 stripPort :: T.Text -> T.Text
 stripPort = T.takeWhile (/= ':')
